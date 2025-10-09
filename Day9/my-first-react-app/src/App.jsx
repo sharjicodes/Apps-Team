716aed1039c8ad66651b45bd705dec87017286c6
Sharjith <sharjithambadiambadi@gmail.com>
2025-10-09 18:49:36 +0530

    movie search app react small change

--- Day9/my-first-react-app/src/App.jsx
+++ Day9/my-first-react-app/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/movie/:id" element={<MovieDetailsPage />} />
+      
     </Routes>
   );
 };
--- Day9/my-first-react-app/src/main.jsx
+++ Day9/my-first-react-app/src/main.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>,
 )
